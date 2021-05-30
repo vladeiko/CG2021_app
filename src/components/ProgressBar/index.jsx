dcadfe801228fb7b19173107d8a75031c573c9c8
yamaha3680 <vlad.yamaha3680@gmail.com>
2021-05-30 14:48:32 +0300

    main-page

---
+++ src/components/ProgressBar/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./style.scss";
+
+const ProgressBar = () => {
+  return(
+    <div className="progress-bar">
+      <span>Ивенты хакатона</span>
+      <img src="https://i.imgur.com/LfzAQAO.png" alt="" />
+    </div>
+  )
+}
+
+export default ProgressBar
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ContentLayout from "../../layouts/ContentLayout";
+import DemoNews from "../../components/DemoNews";
+import MainQuestion from "../../components/MainQuestion";
+import YouTube from "../../components/YouTube";
 
 const TestPage = () => {
-  return <ContentLayout>ABOBA</ContentLayout>;
+  return (
+    <ContentLayout>
+      <DemoNews />
+      <MainQuestion />
+      <YouTube />
+    </ContentLayout>
+  );
 };
 
 export default TestPage;
